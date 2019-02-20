19410263117d4bcb079e254e65bec1e2771836b8
z589app <z589app@gmail.com>
2019-02-21 07:27:37 +0900

    b

---
+++ dist/precache-manifest.d5aeb56699b28d8a3f3157f2f6c58ce2.js
@@ -0,0 +1,79 @@
+self.__precacheManifest = [
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "url": "/static/js/vendor.8d98f356a37e09fab351.js"
+  },
+  {
+    "revision": "ae0be413a4a9d7f4adc596e171e97963",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/static/img/icons/apple-touch-icon.png"
+  },
+  {
+    "url": "/static/js/manifest.2ae2e69a05c33dfc65f8.js"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/static/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/static/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/static/img/icons/mstile-150x150.png"
+  },
+  {
+    "url": "/static/js/app.4987ea0fe99acc83a370.js"
+  },
+  {
+    "revision": "7a2fa8fc2aff558ca7b7e6d5dddb5315",
+    "url": "/static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "4987ea0fe99acc83a370",
+    "url": "/static/css/app.b8e908dd1983cadb5682ced0bf5a9f82.css"
+  },
+  {
+    "revision": "3eeb0096714e4247da5a00488d7713db",
+    "url": "/index.html"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70e2f2177d976dbdb87453fc119f92e0.js"
+  "/precache-manifest.d5aeb56699b28d8a3f3157f2f6c58ce2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa-plank-watch"});
@@ -26,6 +26,19 @@ workbox.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "3eeb0096714e4247da5a00488d7713db"
+  },
+  {
+    "url": "precache-manifest.70e2f2177d976dbdb87453fc119f92e0.js",
+    "revision": "70e2f2177d976dbdb87453fc119f92e0"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "cdaccfcd75916e4d0acea5c0899b4f2b"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
